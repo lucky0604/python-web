a4d11aad1640491c2b7a2610313952f5c51eff12
Lucky <lucky_soft@163.com>
2017-05-19 23:38:20 +0800

    user registration

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget = forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label = 'Password', widget = forms.PasswordInput)
+    password_type = forms.CharField(label = 'Repeat Password', widget = forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password_type(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password_type']:
+            raise forms.ValidationError('Password do not match!')
+        return cd['password_type']
