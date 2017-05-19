eba34acb0acb76a354a961739ac7193f197b4a38
Lucky <lucky_soft@163.com>
2017-05-19 22:21:26 +0800

    add bookmark project and finish basic login view

---
+++ bookmarks/account/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # post views
+#    url(r'^login/$', views.user_login, name = 'login'),
+
+
+    # login / logout views
+    url(r'^login/$', 'django.contrib.auth.views.login', name = 'login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name = 'logout'),
+    url(r'^logout-then-login', 'django.contrib.auth.views.logout_then_login', name = 'logout_then_login'),
+
+    url(r'^$', views.dashboard, name = 'dashboard'),
+
+]
