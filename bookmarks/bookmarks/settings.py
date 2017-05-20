c505d2fdae034c07ac6139d55fb44d3c147f4098
Lucky <lucky_soft@163.com>
2017-05-20 23:56:42 +0800

    ready for user model

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -109,3 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# media directory
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
